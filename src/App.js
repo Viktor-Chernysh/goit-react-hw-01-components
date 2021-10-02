024a26d82b49f25db5e7267dd483c550c8406365
Vitkor Chernysh <viktor.chernysh.work@gmail.com>
2021-10-02 20:22:15 +0300

    Profile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
+import Profile from './components/Profile/Profile';
+import user from './components/Profile/user.json';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
 }
 
 export default App;
