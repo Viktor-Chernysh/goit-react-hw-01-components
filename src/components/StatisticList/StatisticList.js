5e5196bace6148b3e944a65b423eadc4294d8465
Vitkor Chernysh <viktor.chernysh.work@gmail.com>
2021-10-03 21:03:40 +0300

    add colorPicker

--- src/components/StatisticList/StatisticList.js
+++ src/components/StatisticList/StatisticList.js
@@ -1,16 +1,24 @@
 import PropTypes from 'prop-types';
 import s from './StatisticList.module.css';
+
+const colorPicker = () => {
+  const red = Math.round(Math.random() * 255);
+  const green = Math.round(Math.random() * 255);
+  const blue = Math.round(Math.random() * 255);
+  const backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  return backgroundColor;
+};
 function StatisticList({ value }) {
   return (
     <ul className={s.statList}>
       {value.map(({ id, label, percentage }) => (
-        <li className={s.item} key={id}>
+        <li
+          className={s.item}
+          key={id}
+          style={{ backgroundColor: colorPicker() }}
+        >
           <span className={s.label}>{label}</span>
           <span className={s.percentage}>{percentage}%</span>
-          {/* <StatisticItem
-            label={element.label}
-            percentage={element.percentage}
-          /> */}
         </li>
       ))}
     </ul>
@@ -19,7 +27,7 @@ function StatisticList({ value }) {
 export default StatisticList;
 StatisticList.propTypes = {
   value: PropTypes.array.isRequired,
-  label: PropTypes.string.isRequired,
-  percentage: PropTypes.number.isRequired,
+  label: PropTypes.string,
+  percentage: PropTypes.number,
   id: PropTypes.number,
 };
